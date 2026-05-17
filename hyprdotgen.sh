e2dcf55ade24d23cd813ce40effdc7fec0242bc9
Miguel Guion <miguel.chagas614@gmail.com>
2026-05-17 15:06:38 -0300

    OMG HYPRLAND UPDATED AND NOW IT IS DEPRECATED OHNO! I\'LL CREATE A NEW REPO! T-T

--- hyprdotgen.sh
+++ hyprdotgen.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 HYPRDIR=$HOME/.config/hypr
-SETUPDIR=$HOME/GitHub/DESDF/Hyprland
+SETUPDIR=$HOME/GitHub/DotFiles/MyHyprlandConfig/
 
-ln -s $SETUPDIR/defines.conf $HYPRDIR/
-ln -s $SETUPDIR/env_desktop.conf $HYPRDIR/machine.conf
-ln -s $SETUPDIR/sets_desktop.conf $HYPRDIR/environments.conf
-ln -s $SETUPDIR/autostart.conf $HYPRDIR/
-ln -s $SETUPDIR/visual.conf $HYPRDIR/
-ln -s $SETUPDIR/shared_binds.conf $HYPRDIR/
-ln -s $SETUPDIR/shared_rules.conf $HYPRDIR/
+ln -sf $SETUPDIR/defines.conf $HYPRDIR/
+ln -sf $SETUPDIR/env_desktop.conf $HYPRDIR/machine.conf
+ln -sf $SETUPDIR/sets_desktop.conf $HYPRDIR/environments.conf
+ln -sf $SETUPDIR/autostart.conf $HYPRDIR/
+ln -sf $SETUPDIR/visual.conf $HYPRDIR/
+ln -sf $SETUPDIR/shared_binds.conf $HYPRDIR/
+ln -sf $SETUPDIR/shared_rules.conf $HYPRDIR/
 
-ln -s $SETUPDIR/hyprland.conf $HYPRDIR/
+ln -sf $SETUPDIR/hyprland.conf $HYPRDIR/
 
 echo "Hyprland setup done!"
